f3f923e9a5f80a77b0b8199f02a0b80cd914d747
Roman Synovets <HVLLOWses@gmail.com>
2018-10-21 21:34:47 +0300

    init iOS application

---
+++ Brooklyn-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brooklyn-iOS
+//
+//  Created by Roman Synovets on 10/21/18.
+//  Copyright © 2018 Roman Synovets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
